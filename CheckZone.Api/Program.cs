0d67c9858d90dfa4422685c6a8cbf721ae0db2a0
Tùng <tungnvse182691@fpt.edu.vn>
2026-07-06 14:58:55 +0700

    fix: change Discord notification service DI lifetime to Singleton to avoid request scope disposal issue

--- CheckZone.Api/Program.cs
+++ CheckZone.Api/Program.cs
@@ -74,7 +74,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped<IScamReportService, ScamReportService>();
 builder.Services.AddScoped<ILegitProfileService, LegitProfileService>();
-builder.Services.AddScoped<IDiscordNotificationService, DiscordNotificationService>();
+builder.Services.AddSingleton<IDiscordNotificationService, DiscordNotificationService>();
 builder.Services.AddHttpClient();
 
 builder.Services.AddControllers();
